91270240af50115ae2eb111cbc404747649a3536
David Maze <dzmaze@gmail.com>
2012-09-09 16:17:06 -0400

    Split out the database-access code from the rest of the correlation script. Break out functions specifically accessing the FEC data table into a separate class.

--- ftmdb.py
+++ ftmdb.py
@@ -1,40 +1,15 @@
-#!/usr/bin/python
+"""Database-access functions.
 
-import optparse
+These are helpers of various sorts to help you get along with the
+SQLite database that 'csvsetup.py' will create for you.  Generally
+you will need to do something like
+
+from ftmdb import *
 import sqlite3
+db = sqlite3.connect('contributions.sqlite')
+fec = ContributionsFEC(db)
 
-def main():
-    parser = optparse.OptionParser(usage='%prog [options] "candidate"')
-    parser.add_option("-f", "--file", default='contributions.sqlite',
-                      help='Read database file FILE')
-    (options,args) = parser.parse_args()
-    if len(args) != 1:
-        parser.error("Give exactly one candidate name")
-        pass
-    candidate = args[0]
-    db = sqlite3.connect(options.file)
-    q = ContributionsFEC(db)
-    recipients = q.recipients()
-    ts = {}
-    for r in recipients: ts[r] = q.contributions_for_recipient(r)
-    me = ts[candidate]
-    similarities = {}
-    for r in recipients:
-        if r == candidate: continue
-        similarities[r] = similarity(me, ts[r])
-        pass
-    similarities[candidate] = 0.0
-    # sort the list of recipients in descending order by similarity score
-    recipients.sort(None, lambda k: similarities[k], True)
-    #print "Top recipient: {0}".format(recipients[0])
-    #for (k, v1, v2) in common_contributors(me, ts[recipients[0]]):
-    #    print "  {0}: {1} to me, {2} to them".format(k, v1, v2)
-    #    pass
-    print "Top ten:"
-    for r in recipients[:10]:
-        print "  {0}: {1}".format(r, similarities[r])
-        pass
-    pass
+to access specific tables with helpers here."""
 
 class DatabaseQueries(object):
     """Base class to do queries on databases."""
@@ -115,26 +90,3 @@ class ContributionsFEC(DatabaseQueries):
             result[k] = table[k] / total
             pass
         return result
-
-
-def similarity(t1, t2):
-    """Return some sort of numeric index of how similar two tables, keyed
-    by contributor name, are."""
-    # First pass: just count number of keys in common
-    return len(common_contributors(t1, t2))
-
-def common_contributors(t1, t2):
-    """Go through two tables of contributor amounts.  Return tuples of
-    (name, amount1, amount2) for all common contributors."""
-    result = []
-    for k in t1.keys():
-        if k in t2:
-            t = (k, t1[k], t2[k])
-            result.append(t)
-            pass
-        pass
-    return result
-
-if __name__ == '__main__':
-    main()
-    pass
