68c1dd45649723c08a0e1f7810ae82958a6a7db3
David Maze <dzmaze@gmail.com>
2012-09-09 20:42:24 -0400

    passthru.py: do a little more analysis on what's going in Print out lists of * entities with one contributor and one receiver * entities with many contributors and one receiver * entities with one contributor and !one receiver

--- passthru.py
+++ passthru.py
@@ -80,6 +80,10 @@ def main():
     parser = optparse.OptionParser(usage='%prog [options]')
     parser.add_option("-f", "--file", default='contributions.sqlite',
                       help='Read database file FILE')
+    parser.add_option("-i", "--individuals", action="store_true",
+                      help='Include contributions from individuals')
+    parser.add_option("--print-self", action="store_true",
+                      help='Print donors that only give to themselves')
     (options,args) = parser.parse_args()
     if len(args) != 0:
         parser.error("No command-line arguments accepted")
@@ -88,62 +92,112 @@ def main():
     q = ContributionsFEC(db)
 
     # Build a graph of contributors to recipients.
-    # graph is a dictionary keyed on contributor.
-    # Its values are dictionaries based on recipient.
-    # *Its* values are dollar amounts.
+    # Edge values are dollar amounts.
     contributors = Graph()
+    # Do some pre-processing on transaction types:
+    # http://influenceexplorer.com/about/methodology/campaign_finance
+    # suggests only including specific types.
+    valid_types = [ '10', '11', '15', '15e', '15j', '22y',
+                    '24k', '24r', '24z',
+                    'Z90' ]
     def add_row(row):
-        contributors.add(row[0], row[1], float(row[2]))
+        if row[3] in valid_types:
+            contributors.add(row[0], row[1], float(row[2]))
+            pass
         pass
-    q._execute("SELECT contributor_name, recipient_name, amount " +
-               "FROM " + q.table + " " +
-               "WHERE contributor_type='C'",
-               (), add_row)
+    query = ("SELECT contributor_name, recipient_name, amount, " +
+             "transaction_type " + "FROM " + q.table)
+    if not options.individuals:
+        query += " WHERE contributor_type='C'"
+        pass
+    q._execute(query, (), add_row)
     # Find organizations that give to themselves.  Remove those self
     # loops, but also print out the names of organizations that
     # we're removing from the graph this way.
     self_loops = contributors.self_loops()
     self_loops.sort()
-    print "Organizations that only give to themselves:"
+    if options.print_self:
+        print "Organizations that only give to themselves:"
+        pass
     for c in self_loops:
-        if contributors.is_singleton(c):
+        if options.print_self and contributors.is_singleton(c):
             print "  {0}".format(c)
             pass
         contributors.remove(c, c)
         pass
+    if options.print_self:
+        print
+        pass
 
+    # Now:
+    # contributors is a graph from givers to receivers
+    # recipients is a graph from receivers to givers
+    # len(contributors.edges(me)) is the number of people me gives to
+    # contributors.singleton(me) is the person I give to (if only one)
     recipients = contributors.reverse()
 
     contributor_names = contributors.keys()
     contributor_names.sort()
 
-    print
-    print "Contributors with only one recipient and who aren't recipients themselves:"
-    for c in contributor_names:
-        if c not in recipients.keys() and contributors.is_singleton(c):
-            print "  {0}: {1}".format(c, contributors.singleton(c))
+    # At this point, we can look at:
+    # * No contributors (individual)
+    # * No recipients (candidate)
+    # * One contributor (shell)
+    # * One recipient (committee)
+    # * Many contributors
+    # * Many recipients
+    # So let's split both graphs into lists of (zero, one, many):
+    def split_graph(g):
+        zero = []
+        one = []
+        many = []
+        for n in g.keys():
+            l = len(g.edges(n))
+            if l == 0: zero.append(n)
+            elif l == 1: one.append(n)
+            else: many.append(n)
+            pass
+        return (set(zero), set(one), set(many))
+    (candidates, committees, givers) = split_graph(contributors)
+    (individuals, shells, receivers) = split_graph(recipients)
+
+    # The single thing we're most interested in is things with only
+    # one recipient.
+    l = list(committees)
+    l.sort()
+    # "Pass-thru" entities with only one giver
+    print "Pass-thru entities:"
+    for r in l:
+        if r in shells:
+            print ("  {0} (from {1} to {2})"
+                   .format(r, recipients.singleton(r),
+                           contributors.singleton(r)))
             pass
         pass
-
     print
-    print "Contributors who are recipients with only one downstream recipient:"
-    for c in contributor_names:
-        if c in recipients.keys() and contributors.is_singleton(c):
-            print ("  {0}: {1} ({2} contributors)"
-                   .format(c, contributors.singleton(c),
-                           len(recipients.edges(c))))
+
+    print "Committees for a single receiver:"
+    for r in l:
+        if r in receivers:
+            print ("  {0} ({1} contributors, to {2})"
+                   .format(r, len(recipients.edges(r)),
+                           contributors.singleton(r)))
             pass
         pass
-
     print
-    print "Contributors with multiple recipients:"
-    for c in contributor_names:
-        if not contributors.is_singleton(c):
-          print ("  {0} ({1} contributors, {2} recipients)"
-                 .format(c, len(recipients.edges(c)),
-                         len(contributors.edges(c))))
-          pass
+
+    l = list(shells)
+    l.sort()
+    print "Other shells:"
+    for c in l:
+        if c not in committees:
+            print ("  {0} (for {1}, {2} recipients)"
+                   .format(c, recipients.singleton(c),
+                           len(contributors.edges(c))))
+            pass
         pass
+    print
+
     pass
 
 if __name__ == '__main__':
